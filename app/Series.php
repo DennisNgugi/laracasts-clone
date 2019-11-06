65ba9f694cbba8ef7a275ce60df56151e648661f
Dennis Ngugi <denongugi73@gmail.com>
2019-11-06 11:10:37 -0800

    Register blade added together with its functionalities

---
+++ app/Series.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Dennis;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Series extends Model
+{
+    //
+}
---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+  public function test_a_user_recieves_correct_message_when_passing_in_wrong_credentials(){
+    
+  }
+}
