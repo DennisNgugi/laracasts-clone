c3489a8dcbf0c3e9640abba7f00e0d2c7f342fdb
Dennis Ngugi <denongugi73@gmail.com>
2019-11-06 06:31:53 -0800

    First commit

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+
+  <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="card card-shadowed p-50 w-400 mb-0" style="max-width: 100%">
+        <h5 class="text-uppercase text-center">Login</h5>
+        <br><br>
+
+        <form>
+          <div class="form-group">
+            <input type="email" class="form-control"  placeholder="Email">
+          </div>
+
+          <div class="form-group">
+            <input type="password" class="form-control"  placeholder="Password">
+          </div>
+
+          <div class="form-group flexbox py-10">
+            <label class="custom-control custom-checkbox">
+              <input type="checkbox" class="custom-control-input"  checked>
+              <span class="custom-control-indicator"></span>
+              <span class="custom-control-description">Remember me</span>
+            </label>
+
+            <a class="text-muted hover-primary fs-13" href="#">Forgot password?</a>
+          </div>
+
+          <div class="form-group">
+            <button class="btn btn-bold btn-block btn-primary" :disabled="!isValidLoginForm" type="submit">Login</button>
+          </div>
+        </form>
+
+
+
+        <hr class="w-30">
+
+        <p class="text-center text-muted fs-13 mt-20">Don't have an account? <a href="page-register.html">Sign up</a></p>
+      </div>
+
+    </div>
+  </div>
+
+</div>
+</template>
+
+<script>
+    export default {
+      data(){
+        return{
+          email:'',
+          password:'',
+          remember:true
+        }
+
+      },
+
+      computed :{
+        isValidLoginForm(){
+          return this.emailIsValid() && this.password
+        }
+      },
+      methods:{
+        emailIsValid(){
+     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.email))
+     {
+    return (true)
+      }
+    return (false)
+      }
+    },
+        mounted() {
+            console.log('Login mounted.')
+        }
+    }
+</script>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- <meta property="og:title" content="TheSaaS">
+    <meta property="og:description" content="A responsive, professional, and multipurpose SaaS, Software, Startup and WebApp landing template powered by Bootstrap 4.">
+    <meta property="og:image" content="http://thetheme.io/thesaas/assets/img/og-img.jpg">
+    <meta property="og:url" content="http://thetheme.io/thesaas/">
+    <meta name="twitter:card" content="summary_large_image"> -->
+
+    <title>Laracasts clone</title>
+    <link href="/assets/css/core.min.css" rel="stylesheet">
+    <link href="/assets/css/thesaas.min.css" rel="stylesheet">
+    <link href="/assets/css/style.css" rel="stylesheet">
+    <!-- Scripts -->
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div id="app">
+
+      <!-- Topbar -->
+      <nav class="topbar topbar-inverse topbar-expand-md topbar-sticky">
+        <div class="container">
+
+          <div class="topbar-left">
+            <button class="topbar-toggler">&#9776;</button>
+            <a class="topbar-brand" href="index.html">
+              <img class="logo-default" src="/assets/img/logo.png" alt="logo">
+              <img class="logo-inverse" src="/assets/img/logo-light.png" alt="logo">
+            </a>
+          </div>
+
+
+          <div class="topbar-right">
+            <ul class="topbar-nav nav">
+              <li class="nav-item"><a class="nav-link active" href="index.html">Home</a></li>
+              <li class="nav-item"><a class="nav-link" href="javascript:;" data-toggle="modal" data-target="#loginModal">Login</a></li>
+              <!-- <li class="nav-item">
+                <a class="nav-link" href="#">Samples <i class="fa fa-caret-down"></i></a>
+                <div class="nav-submenu">
+                  <a class="nav-link" href="demo-helpato.html">Helpato</a>
+                  <a class="nav-link" href="demo-trello.html">Trello</a>
+                  <a class="nav-link" href="demo-gmail.html">Gmail</a>
+                  <a class="nav-link" href="demo-skype.html">Skype</a>
+                  <a class="nav-link" href="demo-github.html">GitHub</a>
+                  <a class="nav-link" href="demo-app.html">Mobile App</a>
+                  <a class="nav-link" href="demo-bootstrap.html">Bootstrap</a>
+                  <a class="nav-link" href="demo-slack.html">Slack</a>
+                  <a class="nav-link" href="demo-zendesk.html">Zendesk</a>
+                </div>
+              </li> -->
+
+
+
+
+            </ul>
+          </div>
+
+        </div>
+      </nav>
+
+<!-- End top bar -->
+
+<!-- HEader -->
+      @yield('header')
+<!-- end header -->
+        <main class="main-content">
+            @yield('content')
+        </main>
+
+        <vue-login><vue-login>
+
+    </div>
+</body>
+<script src="{{ asset('js/app.js') }}" defer></script>
+<script src="/assets/js/core.min.js"></script>
+<script src="/assets/js/thesaas.min.js"></script>
+<script src="/assets/js/script.js"></script>
+</html>
